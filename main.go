68355bd72672e58ff089e99a44c5c0fd94f7d592
beepscore <support@beepscore.com>
2017-03-13 22:07:05 -0700

    Add main.go

--- main.go
+++ main.go
@@ -1,15 +1,32 @@
 package main
 
 import (
+	"encoding/json"
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
 )
 
+type Person struct {
+	ID        string   `json:"id,omitempty"`
+	Firstname string   `json:"firstname,omitempty"`
+	Lastname  string   `json:"lastname,omitempty"`
+	Address   *Address `json:"address,omitempty"`
+}
+
+type Address struct {
+	City  string `json:"city,omitempty"`
+	State string `json:"state,omitempty"`
+}
+
+// people is a slice of Person
+var people []Person
+
 func GetPersonEndpoint(w http.ResponseWriter, req *http.Request) {
 }
 
 func GetPeopleEndpoint(w http.ResponseWriter, req *http.Request) {
+	json.NewEncoder(w).Encode(people)
 }
 
 func CreatePersonEndpoint(w http.ResponseWriter, req *http.Request) {
@@ -20,6 +37,10 @@ func DeletePersonEndpoint(w http.ResponseWriter, req *http.Request) {
 
 func main() {
 	router := mux.NewRouter()
+
+	// create sample data
+	people = append(people, Person{ID: "1", Firstname: "Nic", Lastname: "Raboy", Address: &Address{City: "Dublin", State: "CA"}})
+	people = append(people, Person{ID: "1", Firstname: "Maria", Lastname: "Raboy"})
 
 	router.HandleFunc("/people", GetPeopleEndpoint).Methods("GET")
 	router.HandleFunc("/people/{id}", GetPersonEndpoint).Methods("GET")
